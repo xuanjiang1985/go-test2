0ae250fa39265287598388d0376e868c2453165f
Yzhougang <sogongyu@163.com>
2020-08-25 12:05:13 +0800

    copy

--- monster/monster_test.go
+++ monster/monster_test.go
@@ -1,1 +1,37 @@
 package monster
+
+import (
+	"testing"
+)
+
+func TestStore(t *testing.T) {
+	monster := &Monster{
+		Name:  "wang",
+		Age:   19,
+		Skill: "da da",
+	}
+
+	err := monster.Store()
+
+	if err != nil {
+		t.Fatalf("monster.Store test err %+v", err)
+	}
+
+	t.Logf("monster.Store() test ok")
+}
+
+func TestRestore(t *testing.T) {
+	monster := &Monster{
+		Name:  "wang",
+		Age:   19,
+		Skill: "da da",
+	}
+
+	err := monster.Restore()
+
+	if err != nil {
+		t.Fatalf("monster.Restore() test err %+v", err)
+	}
+
+	t.Logf("monster.Restore() test ok")
+}
